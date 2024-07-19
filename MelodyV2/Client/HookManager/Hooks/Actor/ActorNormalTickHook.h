e9cc746e78111e1205993bff312716ea3c137f2d
AshleyNeo1 <3066584511@qq.com>
2024-07-19 12:40:36 +0800

    添加项目文件。

---
+++ MelodyV2/Client/HookManager/Hooks/Actor/ActorNormalTickHook.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../../../../SDK/Classes/Actor.h"
+#include "../../../../Utils/MemoryUtils.h"
+
+class ActorNormalTickHook {
+protected:
+	using func_t = void(__fastcall*)(Actor*);
+	static inline func_t func;
+	
+	static void callback(Actor* _this) {
+		//mc.getClientInstance();
+		if (_this == mc.getLocalPlayer()) 
+			if(mc.getClientInstance() != nullptr)
+			client->moduleMgr->onNormalTick(_this);
+		func(_this);
+	}
+public:
+	static void init(uintptr_t address) {
+		MemoryUtils::CreateHook("Actor_NormalTickHook", address, (void*)&ActorNormalTickHook::callback, (void*)&func);
+	}
+};
